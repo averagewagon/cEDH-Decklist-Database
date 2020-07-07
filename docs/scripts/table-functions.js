695084fb5ecee58d97ad184a85d876c0b81c8d7f
AverageDragon <john.r.hendric@gmail.com>
2020-07-06 18:28:03 -0700

    cont

--- docs/scripts/table-functions.js
+++ docs/scripts/table-functions.js
@@ -112,20 +112,26 @@
   function determineFilterCompliance(entryId, fs) {
     let show = true;
     let entry = qs(".m" + entryId).dataset;
+      console.log("liftoff");
     
     if (fs.section != entry.section) {
       show = false;
+      console.log(entry.section);
     } else if (fs.rec && !entry.recommended) {
       show = false;
+      console.log(entry.recommended);
     } else if (fs.primer && entry.primer == "noprimer") {
       show = false;
+      console.log(entry.primer);
     } else if (fs.discord && !entry.discordLink) {
       show = false;
+      console.log(entry.discordLink);
     } else {
       for (let i = 0; i < fs.colors.length; i++) {
         let color = fs.colors[i];
         if (!entry.colors.includes(color)) {
           show = false;
+          console.log(color);
           break;
         }
       }
@@ -137,6 +143,7 @@
           + entry.description + " "
           + entry.discordTitle + " "
           + entry.decks;
+          console.log(match);
         if (fs.search && !match.toUpperCase().includes(fs.search)) {
           show = false;
         }
