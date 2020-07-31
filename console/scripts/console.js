beb762af6ef7d8c2c83ffe05bcbc08b9cf385b9e
AverageDragon <john.r.hendric@gmail.com>
2020-07-30 22:05:01 -0700

    Redesign SVGs to have circles behind them

--- console/scripts/console.js
+++ console/scripts/console.js
@@ -4,10 +4,12 @@
   window.addEventListener("load", init);
 
   // Initialization function
-  function init() {
+  async function init() {
     //if (get("jwt")) {
-      readRequests();
-      readDecks();
+      await readRequests();
+      await readDecks();
+      id("show-deleted").addEventListener("change", toggleRequests);
+      id("content").classList.remove("hidden");
     //}
   }
   
@@ -55,16 +57,20 @@
       }
       alert(" There was an error while fetching requests:\n" + result.message);
     }*/
-    populateRequests([{"category": "Request that a deck is removed","date": "2020-07-30T23:09:41.279Z","description": "adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat ","id": "1596150581279krbl"},{"category": "Add, modify, or remove an entry's decklists","date": "2020-07-30T23:07:38.798Z","description": "inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.","id": "1596150458798hwru"},{"category": "Make a correction to a submission","date": "2020-07-30T23:07:22.019Z","description": "This is an example request. inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciu","id": "15961504420193acm"},{"category": "Add, modify, or remove an entry's decklists","date": "2020-07-30T23:08:30.618Z","description": "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciu","id": "1596150510618mrlx"}]);
+    populateRequests([{"deleted": false,"category": "Request that a deck is removed","date": "2020-07-30T23:09:41.279Z","description": "adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat ","id": "1596150581279krbl"},{"deleted": true,"category": "Add, modify, or remove an entry's decklists","date": "2020-07-30T23:07:38.798Z","description": "inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.","id": "1596150458798hwru"},{"deleted": false,"category": "Make a correction to a submission","date": "2020-07-30T23:07:22.019Z","description": "This is an example request. inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciu","id": "15961504420193acm"},{"deleted": false,"category": "Add, modify, or remove an entry's decklists","date": "2020-07-30T23:08:30.618Z","description": "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciu","id": "1596150510618mrlx"}]);
   }
   
   function populateRequests(requests) {
     const sortedRequests = requests.sort((a, b) => { return b.date.localeCompare(a.date); });
-    const requestList = id("req-list");
+    const requestList = id("requests");
     const template = id("req-template");
     for (let i = 0; i < sortedRequests.length; i++) {
       const req = sortedRequests[i];
       const item = template.cloneNode(true);
+      if (req.deleted) {
+        item.classList.add("deleted", "hidden");
+        item.querySelector(".req-delete").classList.add("hidden");
+      }
       item.classList.add("r" + req.id);
       item.querySelector(".req-category").innerText = req.category;
       item.querySelector(".req-description").innerText = req.description;
@@ -73,7 +79,13 @@
       item.querySelector(".req-delete").addEventListener("click", deleteRequest);
       requestList.appendChild(item);
     }
-    console.log();
+  }
+  
+  function toggleRequests() {
+    const reqs = qsa("#requests li");
+    for (let i = 0; i < reqs.length; i++) {
+      reqs[i].classList.toggle("hidden");
+    }
   }
   
   async function deleteRequest() {
@@ -89,7 +101,9 @@
     if (result.success) {
       console.log(result.message);
       console.log(result.data);
-      qs(".r" + id).remove();
+      qs(".r" + id).style.backgroundColor = "var(--canvas-color)";
+      qs(".r" + id).classList.add("deleted", "hidden");
+      qs(".r" + id + " .req-delete").classList.add("hidden");
     } else {
       console.error(result.message);
       if (result.data) {
