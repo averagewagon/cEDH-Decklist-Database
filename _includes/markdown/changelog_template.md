0b620dcbf8975c69354daaa1c7bcfad17fbda417
AverageDragon <john.r.hendric@gmail.com>
2020-08-06 19:10:44 -0700

    Implement initial Edit Changelog functionality

---
+++ _includes/markdown/changelog_template.md
@@ -0,0 +1,18 @@
+# New Decks
+- [deck name] added to [section]
+
+# New deck links
+- Added [decklist name] to [deck name]
+
+# Modified Decks
+- Renamed [original deck name] to [new name]
+- Added a Discord server to [deck name]
+- Merged [deck name 1] and [deck name 2]
+
+# Removed Decks
+- Deprecated [deck name]
+- Removed [deck name]
+
+# Recommendations
+- Recommended [deck name]
+- No longer recommending [deck name]
---
+++ console/scripts/changelog.js
@@ -0,0 +1,65 @@
+---
+---
+(function() { "use strict";
+window.addEventListener("load", init);
+
+// Initialization function
+function init() {
+  if (get("jwt")) {
+    id("input").addEventListener("keyup", generatePreview);
+    id("input").addEventListener("change", generatePreview);
+    qs("form").addEventListener("submit", submit);
+    qsa("#changelog-posts li").forEach(item => item.addEventListener("click", switchLog));
+    generatePreview();
+    id("content").classList.remove("hidden");
+  }
+}
+
+function generatePreview() {
+  id("preview").innerHTML = snarkdown(id("input").value);
+}
+
+function switchLog() {
+  qsa("#changelog-posts li").forEach(item => item.classList.remove("active"));
+  this.classList.add("active");
+  
+  id("title").value = iqs(this, ".title").innerText;
+  id("input").value = iqs(this, ".markdown").innerText;
+  generatePreview();
+}
+
+async function submit(event) {
+  event.preventDefault();
+  
+  if (!confirm("Are you sure you want to update the changelog?")) {
+    return;
+  }
+  if (!get("jwt")) {
+    alert("You are not logged in. You must be logged in to make these changes.");
+    return;
+  }
+  const id = qs("#changelog-posts .active").id;
+  const body = {
+    "jwt": get("jwt"),
+    "method": "UPDATE_CHANGELOG",
+    "file": id,
+    "title": base64.encode(utf8.encode(id("title").value)),
+    "content": base64.encode(utf8.encode(id("input").value))
+  }
+  const result = await sendToDDB(body);
+  if (result.success) {
+    alert(result.message);
+  } else {
+    if (result.data) {
+      console.error(result.data);
+    }
+    console.error(result.message);
+    alert(result.message);
+  }
+}
+
+/* HELPER FUNCTIONS */
+{% include javascript/dom.js %}
+{% include javascript/backend.js %}
+
+})();
